dcb1a514732cb2e472f985ab56d0b64f8be881eb
andypsh <andypsh@naver.com>
2026-04-10 18:47:31 +0900

    260410  andy push

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,11 @@
-# /streamlit_app.py (최상위 루트에 생성)
+# /main.py (최상위 루트에 생성)
 import os
 import sys
 
 # 실제 코드가 있는 폴더를 경로에 추가
 sys.path.append(os.path.join(os.getcwd(), 'streamlit/src'))
 
-# 진짜 메인 실행 파일(streamlit_app.py)의 로직을 호출하거나
+# 진짜 메인 실행 파일(main.py)의 로직을 호출하거나
 # 해당 경로를 기준으로 다시 실행하게 만듭니다.
 with open("streamlit/src/main.py", "r", encoding="utf-8") as f:
     exec(f.read())
